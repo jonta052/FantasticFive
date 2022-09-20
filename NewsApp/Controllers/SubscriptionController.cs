e65e6685bb0ef728bb32551ed3d5cc1dbc40014f
jonta052 <55479519+jonta052@users.noreply.github.com>
2022-09-20 14:04:59 +0200

    Added Subscription Views and Actions

--- NewsApp/Controllers/SubscriptionController.cs
+++ NewsApp/Controllers/SubscriptionController.cs
@@ -1,102 +1,103 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using NewsApp.Data;
 using NewsApp.Models;
+using NuGet.Protocol;
 
 namespace NewsApp.Controllers
 {
     public class SubscriptionController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
 
-        public SubscriptionController(ApplicationDbContext db)
+        public SubscriptionController(ApplicationDbContext db, UserManager<User> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         // GET: SubscriptionController
         public IActionResult Index()
         {
-            var listOfSubscriptions = _db.Subscriptions.ToList();
-            return View(listOfSubscriptions);
+            var listOfSubscriptionTypes = _db.SubscriptionTypes.ToList();
+            return View(listOfSubscriptionTypes);
         }
 
         // GET: SubscriptionController/Details/5
-        public IActionResult Details(int id)
+        /*public IActionResult Details(int id)
         {
             return View();
-        }
+        }*/
 
         // GET: SubscriptionController/Create
         public IActionResult Create()
         {
-
+            var subscriptionTypes = _db.SubscriptionTypes.ToList();
+            //add price here
+            var selectList = new SelectList(subscriptionTypes, "Id", "TypeName");
+            ViewBag.SubType = selectList;
             return View();
         }
 
         // POST: SubscriptionController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(string subscriptionType)
+        //[ValidateAntiForgeryToken]
+        public IActionResult Create(int subId)
         {
             var subscriptionTypes = _db.SubscriptionTypes.ToList();
-            IEnumerable<SelectListItem> items =
+            var user = _userManager.GetUserAsync(User).Result;
 
-        from item in subscriptionTypes
+            var selectList = new SelectList(subscriptionTypes, "Id", "TypeName");
 
-        select new SelectListItem
+            ViewBag.SubType = selectList;
 
-        {
+            var usr = User;
+            Subscription subscription = new Subscription();
+            var created = subscription.Created;
+            subscription.Expires = created.AddDays(30);
+            subscription.User = user;
+            //subscription.KlarnaOrder = true;
+            subscription.Active = true;
 
-            Text = item.TypeName.ToString(),
-
-            Value = item.Price.ToString()
-
-        };
-            ViewBag.SubType = items;
-            /*if (!ModelState.IsValid)
-            { }*/
-            try
+            foreach (var item in selectList)
             {
-                //subscription = (from s in _db.Subscriptions where s.Id == subId select s).FirstOrDefault();
-                Subscription subscription = new Subscription();
-                subscription.Expires = subscription.Created.AddDays(30);
-                //subscription.KlarnaOrder = true;
-                subscription.Active = true;
-
-                foreach (var item in items)
+                if (item.Value == subId.ToString())
                 {
-                    if (item.Selected == true)
-                    {
-                        subscription.Price = decimal.Parse(item.Value);
-                    }
+                    var thisSubscription = (from s in subscriptionTypes
+                                            where s.Id == decimal.Parse(item.Value)
+                                            select s).FirstOrDefault();
+
+                    subscription.Price = thisSubscription.Price;
+
+                    subscription.SubscriptionType = thisSubscription;
+
+                    subscription.Name = thisSubscription.TypeName;
                 }
-
-                _db.Subscriptions.Add(subscription);
-                _db.SaveChanges();
-
-                return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
-
+            _db.Subscriptions.Add(subscription);
+            _db.SaveChanges();
+            
+            return View();
 
         }
 
         // GET: SubscriptionController/Edit/5
         public IActionResult Edit(int id)
         {
-            return View();
+            var thisSubType = _db.SubscriptionTypes.Where(s => s.Id == id).FirstOrDefault();
+            return View(thisSubType);
         }
 
         // POST: SubscriptionController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(int id, SubscriptionType subType)
         {
             try
             {
+                _db.Update(subType);
+                _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -108,16 +109,19 @@ namespace NewsApp.Controllers
         // GET: SubscriptionController/Delete/5
         public IActionResult Delete(int id)
         {
-            return View();
+            var thisSubType = _db.SubscriptionTypes.Where(s => s.Id == id).FirstOrDefault();
+            return View(thisSubType);
         }
 
         // POST: SubscriptionController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(int id, SubscriptionType subType)
         {
             try
             {
+                _db.Remove(subType);
+                _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
