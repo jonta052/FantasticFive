c765f8368f8174219840945018b6879d7362f014
Yaaldaa <yaaldaa.k@outlook.com>
2022-11-10 11:55:43 +0100

    EditEmail

--- NewsApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ NewsApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -130,7 +130,7 @@ namespace NewsApp.Areas.Identity.Pages.Account.Manage
                     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                 StatusMessage = "Confirmation link to change email sent. Please check your email.";
-                return RedirectToPage();
+                return Redirect(callbackUrl);
             }
 
             StatusMessage = "Your email is unchanged.";
